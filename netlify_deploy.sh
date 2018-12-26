557252551d5b60c988e994121e59f6ea128e9c20
Eric Baranowski <eric@kulado.com>
2018-12-25 21:26:05 -0700

    initial

--- netlify_deploy.sh
+++ netlify_deploy.sh
@@ -9,14 +9,14 @@ if [ "$TRAVIS_BRANCH" = "master" ] ; then
     curl -H "Content-Type: application/zip" \
         -H "Authorization: Bearer $token" \
         --data-binary "@website.zip" \
-        https://api.netlify.com/api/v1/sites/914901f4-03d3-4a52-b3ce-1a499b5f90f5/deploys
+        https://api.netlify.com/api/v1/sites/44259856-784c-4e35-a14c-6dd6174be654/deploys
     echo "Build successful, publishing in production environment!"
 
 elif [ "$TRAVIS_BRANCH" = "develop" ] ; then
     curl -H "Content-Type: application/zip" \
             -H "Authorization: Bearer $token" \
             --data-binary "@website.zip" \
-            https://api.netlify.com/api/v1/sites/47f96b73-9e2b-4d08-9935-767b12fdf79b/deploys
+            https://api.netlify.com/api/v1/sites/44259856-784c-4e35-a14c-6dd6174be654/deploys
     echo "Build successful, publishing in develop environment!"
 
 else
